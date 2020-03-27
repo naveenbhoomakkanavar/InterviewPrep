989ee5fcf21ea382643fdbc4312b830d73386169
naveenbhoomakkanavar <naveenbhoomakkanavar@gmail.com>
2020-03-26 21:29:41 -0700

    Create LargestNumberAtLeastTwiceofOthers.java

---
+++ Arrays/LargestNumberAtLeastTwiceofOthers.java
@@ -0,0 +1,31 @@
+In a given integer array nums, there is always exactly one largest element.
+
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+
+Input: nums = [3, 6, 1, 0]
+Output: 1
+Explanation: 6 is the largest integer, and for every other number in the array x,
+6 is more than twice as big as x.  The index of value 6 is 1, so we return 1.
+
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int maxIndex = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+                maxIndex = i;
+            }
+        }
+        
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] != max && max < 2*nums[i]){
+                return -1;
+            }
+        }
+        return maxIndex;
+    }
+}
